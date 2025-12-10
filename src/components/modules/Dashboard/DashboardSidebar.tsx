55511da72143676c27367f736e709c264885565a
ashraful2871 <ashrafulislam2758@gmail.com>
2025-12-10 20:05:55 +0600

    setup proxy file for route proctation

--- src/components/modules/Dashboard/DashboardSidebar.tsx
+++ src/components/modules/Dashboard/DashboardSidebar.tsx
@@ -1,13 +1,11 @@
 import { getNavItemsByRole } from "@/lib/navItems.config";
 import { getDefaultDashboardRoute } from "@/service/auth/auth-utils";
 import { NavSection } from "@/types/dashboard.interface";
-import React from "react";
 import DashboardSidebarContent from "./DashboardSidebarContent";
 import { getUserInfo } from "@/service/auth/getUserIfo";
 
 const DashboardSidebar = async () => {
   const userInfo = await getUserInfo();
-  console.log(userInfo);
 
   const navItems: NavSection[] = getNavItemsByRole(userInfo.role);
   const dashboardHome = getDefaultDashboardRoute(userInfo.role);
